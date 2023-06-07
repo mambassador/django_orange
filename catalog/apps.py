252cc59c5e2bc0ef69a99faed40e0726cfd11cb2
Vladyslav <vladyslavtkach.qa@gmail.com>
2023-06-08 01:46:56 +0300

    added docs

--- catalog/apps.py
+++ catalog/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
+    """Configuration class for the 'catalog' Django app
+
+    Attributes:
+        default_auto_field (str): The default auto-generated field type
+                                  for models in the app.
+        name (str): The name of the app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "catalog"
