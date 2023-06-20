7b150d4ae8ec520df33a13bfc26617ea637d51be
Vladyslav <vladyslavtkach.qa@gmail.com>
2023-06-21 01:17:00 +0300

    added models for catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
-from django.db import models  # noqa: F401
+from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    products = models.ManyToManyField("Product")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    suppliers = models.ManyToManyField("Supplier")
+
+    def __str__(self):
+        return self.name
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.OneToOneField(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
