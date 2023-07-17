a8d3fb571a8cb505c5914d245cd7a3029891b95a
Vladyslav <vladyslavtkach.qa@gmail.com>
2023-07-18 00:24:07 +0300

    changed beat name

--- core/celery.py
+++ core/celery.py
@@ -19,7 +19,7 @@ app.autodiscover_tasks()
 
 
 app.conf.beat_schedule = {
-    "scrape-every-odd-hour": {
+    "add-every-odd-hour": {
         "task": "quotes.tasks.scrape",
         "schedule": crontab(hour="1-23/2"),
     },
